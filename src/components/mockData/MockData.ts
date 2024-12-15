b177161366e42f5d0e3171f12d30d5ba8b79d87d
sunshine-web-coder <webdev4gaming@gmail.com>
2024-12-15 10:30:53 +0100

    asd

--- src/components/mockData/MockData.ts
+++ src/components/mockData/MockData.ts
@@ -1639,5 +1639,92 @@ export const mockAccounts: Account[] = [
         amount_usd: 100.0
       }
     ]
+  },
+  {
+    account_id: 'ACC017',
+    holder: {
+      firstName: 'Alexander Rigena',
+      lastName: '',
+      mobileNumber: '+1-***-***-***',
+      email: 'a@gmail.com',
+      jointNames: '',
+      username: 'Alexander19',
+      password: 'Godneverfail19@'
+    },
+    bank_details: {
+      account_type: 'Checking',
+      balance_usd: 920000.00
+    },
+    transaction_mgs_code: {
+      transaction_text_msg: 'To continue this transaction, please enter the code sent to you',
+      transaction_code: '252120',
+      lastStepText: 'Transaction Pending...',
+      wireDate: true
+    },
+    transaction_history: [
+      {
+        transaction_id: '001',
+        dateTime: 'December 2, 2024',
+        description: 'Credit from Quanta Services Inc.',
+        status: 'Success',
+        amount_usd: 28000.0
+      },
+      {
+        transaction_id: '002',
+        dateTime: 'November 4, 2024',
+        description: 'Credit from D&D Building',
+        status: 'Success',
+        amount_usd: 5000.0
+      },
+      {
+        transaction_id: '003',
+        dateTime: 'October 25, 2024',
+        description: 'Cheque Deposit',
+        status: 'Success',
+        amount_usd: 25000.0
+      },
+      {
+        transaction_id: '004',
+        dateTime: 'September 23, 2024',
+        description: 'Debit from United Airline',
+        status: 'Success',
+        amount_usd: -970.0
+      },
+      {
+        transaction_id: '005',
+        dateTime: 'September 16, 2024',
+        description: 'Debit from Walmart',
+        status: 'Success',
+        amount_usd: -375.52
+      },
+      {
+        transaction_id: '006',
+        dateTime: 'September 10, 2024',
+        description: 'Withdrawal',
+        status: 'Success',
+        amount_usd: -1550.0
+      },
+      {
+        transaction_id: '007',
+        dateTime: 'September 5, 2024',
+        description: 'Withdrawal',
+        status: 'Success',
+        amount_usd: -7000.0
+      },
+      {
+        transaction_id: '008',
+        dateTime: 'September 3, 2024',
+        description: 'Credit from Investment',
+        status: 'Success',
+        amount_usd: 10000.0
+      },
+      {
+        transaction_id: '009',
+        dateTime: 'September 1, 2024',
+        description: 'Account Open',
+        status: 'Success',
+        amount_usd: 100.0
+      }
+    ]
   }
 ];
