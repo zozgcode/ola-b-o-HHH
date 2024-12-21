3ba5b2596f24ca9f7b538a2eabe5204889010922
sunshine-web-coder <webdev4gaming@gmail.com>
2024-12-21 08:56:28 +0100

    upd

--- src/components/mockData/MockData.ts
+++ src/components/mockData/MockData.ts
@@ -1735,10 +1735,8 @@ export const mockAccounts: Account[] = [
       mobileNumber: '+1-***-***-***',
       email: 'm@gmail.com',
       jointNames: '',
-      username: 'd',
-      password: 'd'
-      // username: 'dmartin',
-      // password: '1977'
+      username: 'dmartin',
+      password: '1977'
     },
     bank_details: {
       account_type: 'Checking',
