ffce5bb5c1d9628337e5e10d5948ed1f53b4ee2d
sunshine-web-coder <webdev4gaming@gmail.com>
2025-01-24 21:39:30 +0100

    dj

--- src/components/dashboard/Profile.tsx
+++ src/components/dashboard/Profile.tsx
@@ -1,29 +1,29 @@
-"use client";
+'use client';
 
-import { Account } from "@/utils/types";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { GoCheckCircleFill } from "react-icons/go";
-import { IoIosArrowBack } from "react-icons/io";
-import Loader from "../Loader";
+import { Account } from '@/utils/types';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { GoCheckCircleFill } from 'react-icons/go';
+import { IoIosArrowBack } from 'react-icons/io';
+import Loader from '../Loader';
 
 export default function Profile() {
   const router = useRouter();
   const [user, setUser] = useState<Account | null>(null);
 
   useEffect(() => {
-    const loggedInUser = localStorage.getItem("loggedInUser");
+    const loggedInUser = localStorage.getItem('loggedInUser');
     if (loggedInUser) {
       setUser(JSON.parse(loggedInUser));
     } else {
-      router.push("/");
+      router.push('/');
     }
   }, [router]);
 
   const handleLogout = () => {
-    localStorage.removeItem("loggedInUser");
-    router.push("/");
+    localStorage.removeItem('loggedInUser');
+    router.push('/');
   };
 
   if (!user) {
@@ -36,10 +36,7 @@ export default function Profile() {
           <IoIosArrowBack />
           <span className="text-[#252525] font-semibold">My Profile</span>
         </Link>
-        <button
-          onClick={handleLogout}
-          className="p-3 py-2 rounded-lg border-none outline-none font-medium border bg-[#d71e28] text-white text-sm"
-        >
+        <button onClick={handleLogout} className="p-3 py-2 rounded-lg border-none outline-none font-medium border bg-[#d71e28] text-white text-sm">
           Sign out
         </button>
       </div>
@@ -54,9 +51,7 @@ export default function Profile() {
           {user.holder.mobileNumber && (
             <div className="flex w-full justify-between items-center">
               <span className="text-[#3f3f3f] font-normal">Phone</span>
-              <span className="text-[#252525] font-medium flex items-center gap-1">
-                {user.holder.mobileNumber}
-              </span>
+              <span className="text-[#252525] font-medium flex items-center gap-1">{user.holder.mobileNumber}</span>
             </div>
           )}
           {user.holder.email && (
@@ -70,13 +65,18 @@ export default function Profile() {
               </span>
             </div>
           )}
-
-          <div className="w-full justify-between hidden items-center">
-            <span className="text-[#3f3f3f] font-normal">Account Number</span>
-            <span className="text-[#252525] font-medium flex items-center gap-1">
-              012****324
-            </span>
-          </div>
+          {user.bank_details.account_number && (
+            <div className="w-full flex justify-between items-center">
+              <span className="text-[#3f3f3f] font-normal">Account Number</span>
+              <span className="text-[#252525] font-medium flex items-center gap-1">{user.bank_details.account_number}</span>
+            </div>
+          )}
+          {user.bank_details.routing_number && (
+            <div className="w-full flex justify-between items-center">
+              <span className="text-[#3f3f3f] font-normal">Routing Number</span>
+              <span className="text-[#252525] font-medium flex items-center gap-1">{user.bank_details.routing_number}</span>
+            </div>
+          )}
         </div>
       </div>
     </div>
